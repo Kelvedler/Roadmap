f8c6c3e7804d27bc0ecd5fb1b9b9b944a4d2e89d
Yaroslav <hnflkmvjcm@gmail.com>
2021-04-24 23:52:25 +0300

    forms practice

--- Django/forms_project/basic_forms/views.py
+++ Django/forms_project/basic_forms/views.py
@@ -7,5 +7,12 @@ def index(request):
 
 
 def form_name_view(request):
-    form = {'form': forms.FormName()}
-    return render(request, 'basic_forms/form_page.html', context=form)
+    form = forms.FormName()
+    if request.method == 'POST':
+        form = forms.FormName(request.POST)
+
+        if form.is_valid():
+            print('Name: ' + form.cleaned_data['name'])
+            print('Email: ' + form.cleaned_data['email'])
+            print('Text: ' + form.cleaned_data['text'])
+    return render(request, 'basic_forms/form_page.html', context={'form': form})
